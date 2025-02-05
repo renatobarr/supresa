5d58e1d3dbce559e942e47688ddf1dc6acf162dd
Renato Barreto <renato.barreto@edu.unifil.br>
2025-02-05 15:08:35 -0300

    Botões de dica e backgrounds adcionados na página2

--- Pagina_inicial/index.js
+++ Pagina_inicial/index.js
@@ -1,31 +1,27 @@
 const quantidadePresentes = 50;
 const container = document.querySelector(".container_presentes");
 
-// Definir os valores de velocidade mínima e máxima
 const velocidadeMinima = 0.3;
 const velocidadeMaxima = 10;
 
-// Posições do botão
 const botaoStart = document.querySelector('.start');
 const botaoStartRect = botaoStart.getBoundingClientRect();
 const botaoCentroX = botaoStartRect.left + botaoStartRect.width / 2;
 const botaoCentroY = botaoStartRect.top + botaoStartRect.height / 2;
 
 function calcularVelocidadeComBaseNaDistancia(x, y) {
-    // Calcular a distância entre o cursor e o centro do botão
+    
     const distancia = Math.sqrt(Math.pow(x - botaoCentroX, 2) + Math.pow(y - botaoCentroY, 2));
     
-    // Distância máxima entre o cursor e o botão (considerando o tamanho da tela)
     const distanciaMaxima = Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2));
 
-    // Mapear a distância para a velocidade
-    const fatorVelocidade = distancia / distanciaMaxima; // Valor entre 0 e 1
+    const fatorVelocidade = distancia / distanciaMaxima; 
     const velocidade = velocidadeMinima + fatorVelocidade * (velocidadeMaxima - velocidadeMinima);
 
     return velocidade;
 }
 
-// Atualizar a velocidade com base na posição do cursor
+
 let fatorVelocidade = velocidadeMinima;
 
 document.addEventListener("mousemove", (event) => {
@@ -46,7 +42,7 @@ function criarPresente() {
     let rotacaoVelocidade = Math.random() * 4 - 2;
 
     function animar() {
-        posicaoY += velocidadeY * fatorVelocidade; // Multiplicar pela velocidade calculada
+        posicaoY += velocidadeY * fatorVelocidade; 
         posicaoX += velocidadeX;
         rotacao += rotacaoVelocidade;
 
@@ -69,7 +65,19 @@ function criarPresente() {
     animar();
 }
 
-// Criar os presentes
 for (let i = 0; i < quantidadePresentes; i++) {
     criarPresente();
+}
+
+function animacaoButao(){
+    var button = document.getElementById('start')
+    button.classList.remove('animacaoButao');
+    void button.offsetWidth;
+    button.classList.add('animacaoButao');
+
+    setTimeout(function(){
+        window.location.href = '../Pagina_Escolha/escolha.html';
+    },300);
+
+
 }
