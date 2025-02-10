b9d3782521c421c8b0eee8900f73a67a9ecece83
Laboratório de Informática <labinfo@unifil.br>
2025-02-10 20:20:57 -0300

    Testando

---
+++ Pagina_Escolha/escolha.js
@@ -0,0 +1,6 @@
+function dicaHyTech(){
+
+    var buttonMaterialista = document.getElementById('dicamaterialista')
+    var buttonRomantico = document.getElementById('dicaromantico')
+    var buttonHighTech = document.getElementById('dicahightech')
+}
