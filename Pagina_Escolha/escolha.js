45069e5f5cc9765396ced7b85d5274c842f09be4
Renato Barreto <renato.barreto@edu.unifil.br>
2025-02-14 20:46:14 -0300

    animação da box de confirmação

--- Pagina_Escolha/escolha.js
+++ Pagina_Escolha/escolha.js
@@ -39,10 +39,14 @@ document.getElementById('dicahyghtech').addEventListener('click', animacaoboxHyg
 function animacaoHyghTech(){
 
     var buttonHyghTech = document.getElementById('hyghtech')
+    var tela = document.querySelector('.tela')
 
     buttonHyghTech.classList.remove('animacaoPulse');
     void buttonHyghTech.offsetWidth;
     buttonHyghTech.classList.add('animacaoPulse');
+    tela.classList.toggle('visivel');
+
+
     
 }
 document.getElementById('hyghtech').addEventListener('click', animacaoHyghTech);
@@ -50,10 +54,13 @@ document.getElementById('hyghtech').addEventListener('click', animacaoHyghTech);
 function animacaoRomantico(){
 
     var button = document.getElementById('romantico')
+    var tela = document.querySelector('.tela')
+
 
     button.classList.remove('animacaoPulse');
     void button.offsetWidth;
     button.classList.add('animacaoPulse');
+    tela.classList.toggle('visivel');
     
 }
 document.getElementById('romantico').addEventListener('click', animacaoRomantico);
@@ -61,12 +68,38 @@ document.getElementById('romantico').addEventListener('click', animacaoRomantico
 function animacaoMaterialista(){
 
     var button = document.getElementById('materialista')
+    var tela = document.querySelector('.tela')
+
+    button.classList.remove('animacaoPulse');
+    void button.offsetWidth;
+    button.classList.add('animacaoPulse');
+    tela.classList.toggle('visivel');
+    boxConfirmacao.classList.toggle('animacaoScale');
+    
+}
+document.getElementById('materialista').addEventListener('click', animacaoMaterialista);
+
+function animacaoSim(){
+
+    var button = document.getElementById('confirmarButton')
 
     button.classList.remove('animacaoPulse');
     void button.offsetWidth;
     button.classList.add('animacaoPulse');
     
 }
-document.getElementById('materialista').addEventListener('click', animacaoMaterialista);
+document.getElementById('confirmarButton').addEventListener('click', animacaoSim);
+
+function animacaoNao(){
+
+    var button = document.getElementById('cancelarButton')
+
+    button.classList.remove('animacaoPulse');
+    void button.offsetWidth;
+    button.classList.add('animacaoPulse');
+    
+}
+document.getElementById('cancelarButton').addEventListener('click', animacaoNao);
+
 
 
