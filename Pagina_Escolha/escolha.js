cf0ccbf23ff4ebc4789b2a1661ee1fe1c4a16a5f
Renato Barreto <renato.barreto@edu.unifil.br>
2025-02-16 11:03:48 -0300

    Criando efeitos das divs e o box informativo

--- Pagina_Escolha/escolha.js
+++ Pagina_Escolha/escolha.js
@@ -1,3 +1,14 @@
+
+window.onload = function() {
+    setTimeout(() => {
+        var boxInformacao = document.querySelector('.boxInformacao');
+        boxInformacao.classList.remove('animacaoScaleSaida');
+        void boxInformacao.offsetWidth;
+        boxInformacao.classList.add('animacaoScale');
+        document.querySelector('.tela').classList.add('visivel');
+    }, 500); 
+};
+
 function animacaoboxMaterialista(){
 
     var buttonMaterialista = document.getElementById('dicamaterialista')
@@ -36,48 +47,40 @@ function animacaoboxHyghTech(){
 }
 document.getElementById('dicahyghtech').addEventListener('click', animacaoboxHyghTech);
 
-function animacaoHyghTech(){
+function exibirBoxConfirmacao(tipo) {
 
-    var buttonHyghTech = document.getElementById('hyghtech')
-    var tela = document.querySelector('.tela')
+    var tela = document.querySelector('.tela');
+    var boxConfirmacao = document.querySelector('.boxConfirmacao');
+    var textoConfirmacao = document.querySelector('.textoConfirmacao');
+    var button; 
 
-    buttonHyghTech.classList.remove('animacaoPulse');
-    void buttonHyghTech.offsetWidth;
-    buttonHyghTech.classList.add('animacaoPulse');
-    tela.classList.toggle('visivel');
+    // Identifica o botão clicado e define a mensagem correspondente
+    if (tipo === 'materialista') {
+        button = document.getElementById('materialista');
+        textoConfirmacao.innerHTML = 'Você optou pela opção <b style="color: rgb(21, 255, 0);">Materialista</b>, é isso mesmo cretininha?';
+    } else if (tipo === 'romantico') {
+        button = document.getElementById('romantico');
+        textoConfirmacao.innerHTML = 'Você optou pela opção <b style="color: rgb(255, 0, 170);">Romântico</b>, é isso mesmo cretininha?';
+    } else if (tipo === 'hytech') {
+        button = document.getElementById('hyghtech');
+        textoConfirmacao.innerHTML = 'Você optou pela opção <b style="color: rgb(0, 251, 255);">HighTech</b>, é isso mesmo cretininha?';
+    }
 
-
-    
-}
-document.getElementById('hyghtech').addEventListener('click', animacaoHyghTech);
-
-function animacaoRomantico(){
-
-    var button = document.getElementById('romantico')
-    var tela = document.querySelector('.tela')
-
-
-    button.classList.remove('animacaoPulse');
-    void button.offsetWidth;
-    button.classList.add('animacaoPulse');
-    tela.classList.toggle('visivel');
-    
-}
-document.getElementById('romantico').addEventListener('click', animacaoRomantico);
-
-function animacaoMaterialista(){
-
-    var button = document.getElementById('materialista')
-    var tela = document.querySelector('.tela')
 
     button.classList.remove('animacaoPulse');
-    void button.offsetWidth;
+    void button.offsetWidth; 
     button.classList.add('animacaoPulse');
-    tela.classList.toggle('visivel');
-    boxConfirmacao.classList.toggle('animacaoScale');
-    
+
+  
+    boxConfirmacao.classList.remove('animacaoScaleSaida');
+    void boxConfirmacao.offsetWidth;
+    tela.classList.add('visivel');
+    boxConfirmacao.classList.add('animacaoScale');
 }
-document.getElementById('materialista').addEventListener('click', animacaoMaterialista);
+
+document.getElementById('materialista').addEventListener('click', () => exibirBoxConfirmacao('materialista'));
+document.getElementById('romantico').addEventListener('click', () => exibirBoxConfirmacao('romantico'));
+document.getElementById('hyghtech').addEventListener('click', () => exibirBoxConfirmacao('hytech'));
 
 function animacaoSim(){
 
@@ -90,16 +93,40 @@ function animacaoSim(){
 }
 document.getElementById('confirmarButton').addEventListener('click', animacaoSim);
 
-function animacaoNao(){
 
-    var button = document.getElementById('cancelarButton')
+function animacaoNao() {
+    var tela = document.querySelector('.tela');
+    var boxConfirmacao = document.querySelector('.boxConfirmacao');
+    var buttonCancelar = document.getElementById('cancelarButton');
 
-    button.classList.remove('animacaoPulse');
-    void button.offsetWidth;
-    button.classList.add('animacaoPulse');
-    
+    buttonCancelar.classList.remove('animacaoPulse');
+    void buttonCancelar.offsetWidth;
+    buttonCancelar.classList.add('animacaoPulse');
+
+    boxConfirmacao.classList.remove('animacaoScale');
+    void boxConfirmacao.offsetWidth;
+    tela.classList.remove('visivel');
+    boxConfirmacao.classList.add('animacaoScaleSaida');
 }
+
 document.getElementById('cancelarButton').addEventListener('click', animacaoNao);
+
+function animacaoNaoInf() {
+    var tela = document.querySelector('.tela');
+    var boxConfirmacao = document.querySelector('.boxInformacao');
+    var buttonCancelar = document.getElementById('informarButtonInf');
+
+    buttonCancelar.classList.remove('animacaoPulse');
+    void buttonCancelar.offsetWidth;
+    buttonCancelar.classList.add('animacaoPulse');
+
+    boxConfirmacao.classList.remove('animacaoScale');
+    void boxConfirmacao.offsetWidth;
+    tela.classList.remove('visivel');
+    boxConfirmacao.classList.add('animacaoScaleSaida');
+}
+
+document.getElementById('informarButtonInf').addEventListener('click', animacaoNaoInf);
 
 
 
